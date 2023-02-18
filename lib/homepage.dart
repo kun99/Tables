e9ad1ac5f44a8b9113c917c0b862d427017058d9
kun99 <kuns.km@gmail.com>
2023-02-18 18:25:07 +0700

    implemented a semi-working shared preferences, made use of lists for floatingactionbutton

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +15,13 @@ final _homeworkController = TextEditingController();
 final _courseController = TextEditingController();
 final _dateController = TextEditingController();
 
+List<String> _labelOnes = ['Homework', 'Subscriptions'];
+List<String> _labelTwos = ['Course name',  'Company name'];
+List<String> _labelThrees = ['Due date', 'Starting'];
+
+final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+late Future<List> _table;
+
 List<Widget> _tablePages = <Widget> [
   const HomeworksPage(),
   const SubscriptionsPage(),
@@ -23,6 +32,27 @@ var test = {};
 int _selectedIndex = 0;
 
 class _HomePageState extends State<HomePage> {
+
+  Future<void> _addRow(String row) async {
+    final SharedPreferences prefs = await _prefs;
+    final List<String> table = prefs.getStringList('table') ?? [];
+    table.add(row);
+
+    setState(() {
+      _table = prefs.setStringList('table', table).then((bool success) {
+        return table;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _table = _prefs.then((SharedPreferences prefs) {
+      //prefs.clear();
+      return prefs.getStringList('table') ?? [];
+    });
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -44,116 +74,65 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          if(_selectedIndex==0){
-            showDialog<String>(
-              context: context,
-              builder: (BuildContext context) => AlertDialog(
-                title: const Text('Add a homework'),
-                content: Column(
-                    children: <Widget>[
-                      TextField(
-                        controller: _homeworkController,
-                        decoration: const InputDecoration(
-                            labelText: "Homework"
-                        ),
+          showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Add new'),
+              content: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: _homeworkController,
+                      decoration: InputDecoration(
+                          labelText: _labelOnes[_selectedIndex]
                       ),
-                      TextField(
-                        controller: _courseController,
-                        decoration: const InputDecoration(
-                            labelText: "Course name"
-                        ),
+                    ),
+                    TextField(
+                      controller: _courseController,
+                      decoration: InputDecoration(
+                          labelText: _labelTwos[_selectedIndex]
                       ),
-                      TextField(
-                          controller: _dateController,
-                          decoration: const InputDecoration(
-                            icon: Icon(Icons.calendar_today),
-                            labelText: "Due date",
-                          ),
-                          onTap: () async {
-                            DateTime? pickedDate = await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime.now(),
-                                lastDate: DateTime(2030)
-                            );
-                            if(pickedDate != null ){
-                              String formattedDate = DateFormat.yMMMd().format(pickedDate);
-                              setState(() {
-                                _dateController.text = formattedDate;
-                              });
-                            }
+                    ),
+                    TextField(
+                        controller: _dateController,
+                        decoration: InputDecoration(
+                          icon: const Icon(Icons.calendar_today),
+                          labelText: _labelThrees[_selectedIndex],
+                        ),
+                        onTap: () async {
+                          DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime.now(),
+                              lastDate: DateTime(2030)
+                          );
+                          if(pickedDate != null ){
+                            String formattedDate = DateFormat.yMMMd().format(pickedDate);
+                            setState(() {
+                              _dateController.text = formattedDate;
+                            });
                           }
-                      )
-                    ]
+                        }
+                    )
+                  ]
                 ),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () => Navigator.pop(context, 'Cancel'),
-                    child: const Text('Cancel'),
-                  ),
-                  TextButton(
-                    onPressed: () => Navigator.pop(context, 'OK'),
-                    child: const Text('OK'),
-                  ),
-                ],
-              ),
-            );
-          }
-          else if(_selectedIndex==1){
-            showDialog<String>(
-              context: context,
-              builder: (BuildContext context) => AlertDialog(
-                title: const Text('Add a subscription'),
-                content: Column(
-                    children: <Widget>[
-                      TextField(
-                        controller: _homeworkController,
-                        decoration: const InputDecoration(
-                            labelText: "Subscription"
-                        ),
-                      ),
-                      TextField(
-                        controller: _courseController,
-                        decoration: const InputDecoration(
-                            labelText: "Company name"
-                        ),
-                      ),
-                      TextField(
-                          controller: _dateController,
-                          decoration: const InputDecoration(
-                            icon: Icon(Icons.calendar_today),
-                            labelText: "Starting from",
-                          ),
-                          onTap: () async {
-                            DateTime? pickedDate = await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(2020),
-                                lastDate: DateTime(2030)
-                            );
-                            if(pickedDate != null ){
-                              String formattedDate = DateFormat.yMMMd().format(pickedDate);
-                              setState(() {
-                                _dateController.text = formattedDate;
-                              });
-                            }
-                          }
-                      )
-                    ]
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'Cancel'),
+                  child: const Text('Cancel'),
                 ),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () => Navigator.pop(context, 'Cancel'),
-                    child: const Text('Cancel'),
-                  ),
-                  TextButton(
-                    onPressed: () => Navigator.pop(context, 'OK'),
-                    child: const Text('OK'),
-                  ),
-                ],
-              ),
-            );
-          }
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _addRow(_homeworkController.text+_courseController.text+_dateController.text);
+                      Navigator.pop(context, 'OK');
+                      print(_homeworkController.text+_courseController.text+_dateController.text);
+                    });
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          );
         },
         backgroundColor: Colors.green,
         child: const Icon(Icons.add),
@@ -190,7 +169,7 @@ class _HomeworksPageState extends State<HomeworksPage> {
     return Column(
       children: <Widget>[
         Row(
-            children: const <Widget> [
+            children: <Widget> [
               Text('Number'),
               Text('Homework'),
               Text('Course'),
@@ -198,6 +177,34 @@ class _HomeworksPageState extends State<HomeworksPage> {
               Text('Priority'),
             ]
         ),
+        Row(
+          children: [
+            FutureBuilder(
+              future: _table,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                  case ConnectionState.active:
+                  case ConnectionState.done:
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    }
+                    else {
+                      // List table = snapshot.data;
+                      // String entries = "";
+                      // for(int i=0; i < table.length; i++){
+                      //   '${entries + table[i]}\n';
+                      // }
+                      return Text(
+                          '${snapshot.data}\n\n'
+                      );
+                    }
+                }
+              },
+            )
+          ],
+        )
       ],
     );
   }
